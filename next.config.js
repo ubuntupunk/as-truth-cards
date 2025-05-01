4e498e94a8c50e1982faa5d2fca677eb2a62a15a
David Robert Lewis <ubuntupunk@gmail.com>
2025-05-01 13:45:55 -0400

    more linting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
-   reactStrictMode: true,
+  reactStrictMode: true,
+  serverExternalPackages: ['@stackframe/stack-shared']
 }
 
 export default nextConfig
